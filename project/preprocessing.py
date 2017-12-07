8602d13667ce0231f8b390bfb8af9b319994ea4c
anhtubkhnk58 <anhtu23795@gmail.com>
2017-12-07 12:21:15 +0700

    edit word2vec with extra data

--- project/preprocessing.py
+++ project/preprocessing.py
@@ -1,18 +1,73 @@
-import  re
+import re
+import os
+import csv
 
 def pre_processing():
-    f = open('./data/preprocessing/iphone_train_vntokenizer','r')
-    f_p = open('./data/preprocessing/iphone_train_word2vec','w')
+    '''f = open('./data/SA_IT4868_20171_data/iphone_dev','r')
+    f_p = open('./data/preprocessing/iphone_dev','w')
 
     for line in f:
-        new_line = re.sub(r'\d###',"",line)
-        new_line = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-                          ,'url_link',new_line)
-        new_line = re.sub('www[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-                          ,'url_link',new_line)
+        words = line.split()
+        for word_index, word in enumerate(words):
+            if 'http' in word or 'www' in word:
+                words[word_index] = "urllink"
+
+        new_line = ' '.join(words)
+
         f_p.write(new_line.lower())
+        f_p.write('\n')
 
     f.close()
     f_p.close()
+
+    os.chdir("./data/vn.hus.nlp.tokenizer-4.1.1-bin")
+    os.system("sh vnTokenizer.sh -i ../preprocessing/iphone_dev -o ../preprocessing/iphone_dev_tokenizer")
+    os.chdir("../..")
+    '''
+
+    #f = open('./data/preprocessing/iphone_dev_tokenizer','r')
+    #f2 = open('./data/preprocessing/iphone_dev','w')
+    f3 = open('./data/stop_words','r')
+    stop_words = []
+    for word in f3:
+        stop_words.append(word)
+    f3.close()
+
+    f4w = open('./data/preprocessing/train', 'w')
+
+    f4 = open('./data/TRAIN.csv','r')
+    readCSV4 = csv.reader(f4, delimiter=',')
+    for row in readCSV4:
+        new_line = row[1].lower()
+        label = row[0]
+        new_line = re.sub('[\.,!-^@:?=#\[\]()/\d"]', '', new_line)
+        words = new_line.split()
+        for word_index, word in enumerate(words):
+            if word in stop_words:
+                words[word_index] = " "
+            if len(word) < 2 or len(word) > 10:
+                words[word_index] = " "
+
+        new_line = " ".join(words)
+        f4w.write(label+" "+new_line+"\n")
+    f4.close()
+
+    f5 = open('./data/TEST.csv','r')
+    readCSV5 = csv.reader(f5,delimiter=',')
+    for row in readCSV5:
+        new_line = row[1].lower()
+        label = row[0]
+        new_line = re.sub('[\.,!-^@:?=#\[\]()/\d"]', '', new_line)
+        words = new_line.split()
+        for word_index, word in enumerate(words):
+            if word in stop_words:
+                words[word_index] = " "
+            if len(word) < 2 or len(word) > 10:
+                words[word_index] = " "
+        new_line = " ".join(words)
+        f4w.write(label+" "+new_line+"\n")
+
+    f5.close()
+    f4w.close()
 
 pre_processing()
--- project/word2vec.py
+++ project/word2vec.py
@@ -1,27 +1,73 @@
 import numpy as np
+import pickle
+import csv
 import tensorflow as tf
+import os
 
 # read data and build vocabulary
-data_raw = open('./data/preprocessing/iphone_train_vntokenizer','r')
+data_raw = open('./data/preprocessing/iphone_train','r')
+data_raw2 = open('./data/preprocessing/train','r')
 
 words = []
 raw_sentences = []
 
 for line in data_raw:
-    for word in line.split():
-        if word != '.' and len(word) > 1:
-            words.append(word)
+    line_words = line.split()
+    for i in range(1,len(line_words)):
+        words.append(line_words[i])
 
-    for sentence in line.split('.'):
-        raw_sentences.append(sentence)
+    sentence = " ".join(line_words[1:])
+    raw_sentences.append(sentence)
 
 data_raw.close()
 
+
+for line in data_raw2:
+    line_words = line.split()
+    for i in range(1,len(line_words)):
+        words.append(line_words[i])
+
+    sentence = " ".join(line_words[1:])
+    raw_sentences.append(sentence)
+
+data_raw2.close()
+
+
+'''
+data_raw2 = open('./data/TRAIN.csv')
+readCSV2 = csv.reader(data_raw2,delimiter=',')
+for row in readCSV2:
+    line_words = row[1].lower().split()
+    for i in range(len(line_words)):
+        words.append(line_words[i])
+
+    raw_sentences.append(row[1].lower())
+
+data_raw2.close()
+
+data_raw3 = open('./data/TEST.csv')
+readCSV3 = csv.reader(data_raw3,delimiter=',')
+for row in readCSV3:
+    line_words = row[1].lower().split()
+    for i in range(len(line_words)):
+        words.append(line_words[i])
+
+    raw_sentences.append(row[1].lower())
+
+data_raw3.close()
+'''
+
 words = set(words)
+
+f_words = open("words.txt",'w')
+for word in words:
+    f_words.write("%s \n" % word)
+f_words.close()
+
 vocab_size = len(words)
 print('vocabulary size = '+str(vocab_size))
 
-
+'''
 # create a dictionary which translates words to integers and integers to words
 word2int = {}
 int2word = {}
@@ -40,8 +86,8 @@ for sentence in raw_sentences:
 # generate training data (words)
 data = []
 
-WINDOW_SIZE = 2
-
+WINDOW_SIZE = 3
+print('generate training data (words) ...')
 for sentence in sentences:
     for word_index, word in enumerate(sentence):
         for nb_word in sentence[max(word_index-WINDOW_SIZE,0) :
@@ -78,42 +124,79 @@ y_ = tf.placeholder(dtype=tf.float32,shape=(None,vocab_size))
 
 EMBEDDING_DIM = 100
 W1 = tf.Variable(tf.truncated_normal([vocab_size,EMBEDDING_DIM],stddev=0.01, mean=0.0))
-b1 = tf.Variable(tf.truncated_normal([EMBEDDING_DIM],stddev=0.01, mean=0.0))
+b1 = tf.Variable(tf.zeros([EMBEDDING_DIM]))
 
 hidden_presentation = tf.add(tf.matmul(x,W1),b1)
 
 W2 = tf.Variable(tf.random_normal([EMBEDDING_DIM,vocab_size],stddev=0.01, mean=0.0))
-b2 = tf.Variable(tf.random_normal([vocab_size],stddev=0.01, mean=0.0))
+b2 = tf.Variable(tf.zeros([vocab_size]))
 
 prediction = tf.nn.softmax(tf.add(tf.matmul(hidden_presentation,W2),b2))
 
-sess = tf.Session()
-
-init = tf.global_variables_initializer()
-
-sess.run(init)
-
 loss = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(prediction), reduction_indices=[1]))
 
-train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+global_step = tf.contrib.framework.get_or_create_global_step()
+learning_rate = tf.train.exponential_decay(7.5, global_step, 10000, 0.65, staircase=False)
+train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss,global_step)
 
-n_epochs = 100
+n_epochs = 1000
 n_train_samples = len(x_train)
 batch_size = 256
 
-for epoch in range(n_epochs):
+saver = tf.train.Saver()
+save_dir = "save/word2vec"
+save_path = os.path.join(save_dir,"best_model")
 
-    perm = np.random.permutation(len(x_train))
-    ep_loss = 0.0
-    for i in range(0, n_train_samples , batch_size):
+def train():
 
-        x_batch = x_train[perm[i:i+batch_size]]
-        y_batch = y_train[perm[i:i+batch_size]]
+    with tf.Session() as sess:
 
-        _, batch_loss = sess.run([train_step, loss], feed_dict={x:x_batch, y_: y_batch})
+        save_model_path = os.path.join(save_dir,"best_model.index")
+        if not os.path.isfile(save_model_path):
+            print ("Create new model")
+            init = tf.global_variables_initializer()
+            sess.run(init)
+        else:
+            print ("Load exist model")
+            saver.restore(sess=sess, save_path=save_path)
 
-        ep_loss += batch_loss*len(x_batch)
+        min_loss = 10
 
-    print("epoch "+str(epoch)+"/"+str(n_epochs)+"    loss = "+str(ep_loss/n_train_samples))
+        for epoch in range(n_epochs):
 
-vectors = sess.run(W1+b1)
+            perm = np.random.permutation(len(x_train))
+            ep_loss = 0.0
+            print("learning_rate = "+str(learning_rate.eval()))
+            for i in range(0, n_train_samples , batch_size):
+
+                x_batch = x_train[perm[i:i+batch_size]]
+                y_batch = y_train[perm[i:i+batch_size]]
+
+                _, batch_loss = sess.run([train_step, loss], feed_dict={x:x_batch, y_: y_batch})
+
+                ep_loss += batch_loss*len(x_batch)
+
+            ep_loss = ep_loss/n_train_samples
+            print("epoch "+str(epoch)+"/"+str(n_epochs)+"    loss = "+str(ep_loss))
+            if(ep_loss < min_loss):
+                #print("Save model")
+                min_loss = ep_loss
+                saver.save(sess=sess,save_path=save_path)
+
+def get_result():
+    # save result
+    with tf.Session() as sess:
+        saver.restore(sess=sess, save_path=save_path)
+        vectors = sess.run(W1+b1)
+        np.save('./data/word2vec/vectors.npy',vectors)
+        f_word2int = open('./data/word2vec/word2int.pkl','wb')
+        f_int2word = open('./data/word2vec/int2word.pkl','wb')
+        pickle.dump(word2int,f_word2int,pickle.HIGHEST_PROTOCOL)
+        pickle.dump(int2word,f_int2word,pickle.HIGHEST_PROTOCOL)
+        f_int2word.close()
+        f_word2int.close()
+
+
+train()
+
+'''
